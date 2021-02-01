2100efde49cd6c6f92025801388cadb4bec7dff9
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-02-01 17:17:03 -0600

    Get snake body to follow previous body location

--- js/game.js
+++ js/game.js
@@ -2,8 +2,19 @@ let canvas;
 let snakeX = 0;
 let snakeY = 0;
 let direction = 0;
+let snakeLength = 20;
 const movement = 20;
 let firstKey = true;
+const snakeHead = {
+  x: snakeX,
+  y: snakeY
+}
+
+let appleLocation = {
+  x: 200,
+  y: 100
+}
+const snake = [snakeHead];
 
 const colorRectangle = (leftX, topY, width, height, color) => {
   canvasContext.fillStyle = color;
@@ -12,28 +23,62 @@ const colorRectangle = (leftX, topY, width, height, color) => {
 
 const draw = () => {
   colorRectangle(0,0, canvas.width, canvas.height, 'green')
-  colorRectangle(snakeX,snakeY, 20, 20, 'black')
+
+
+  // Apple
+  colorRectangle(appleLocation.x, appleLocation.y, 20, 20, 'red')
+  
+
+  // Snake
+    snake.forEach((section, index) => {
+      // if (index === 0) {
+      //   colorRectangle(section.x, section.y, 20, 20, 'black')
+      //   console.log(section.x )
+      // } else {
+      //   snake[index] = snake[index - 1];
+      //   colorRectangle(section.x, section.y, 20, 20, 'black')
+      // }
+      colorRectangle(section.x, section.y, 20, 20, 'black')
+      console.log('in foreach');
+    
+    
+  });
+  // colorRectangle(snakeX, snakeY, 20, 20, 'black')
+
 }
 
 const move = () => {
-  if ( firstKey) {
-    snakeX += movement;
+  // array of last positions
+  let lastPosition = snake.map((position) => {
+    return {x: position.x, y: position.y}
+  });
+
+  if (firstKey) {
+    snakeHead.x += movement;
   } else {
     switch (direction) {
       case 37:
-        snakeX -= movement;
+        snakeHead.x-= movement;
         break;
       case 39:
-        snakeX += movement;
+        snakeHead.x += movement;
         break;
 
       case 38:
-        snakeY -= 20;
+        snakeHead.y -= 20;
         break;
       case 40:
-        snakeY += 20;
+        snakeHead.y += 20;
         break;
     }
+  }
+
+  for (let i = snake.length - 1; i > 0; i--) {
+    snake[i] = lastPosition[i-1];
+  }
+
+  if (snakeHead.x === appleLocation.x && snakeHead.y === appleLocation.y) {
+    snake.push({x:snake[snake.length - 1].x, y:snake[snake.length -1].y});
   }
   
 }
@@ -43,7 +88,7 @@ window.onload = function () {
   canvas = document.querySelector('#canvas');
       canvasContext = canvas.getContext('2d');
 
-      let framesPerSecond = 5;
+      let framesPerSecond = 1;
       setInterval(() => {
       move();
       draw();
